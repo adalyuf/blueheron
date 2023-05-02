37cb15b8108e4d53d7ff5f775c3c1c80e0b46cdd
adalyuf <adalyuf@gmail.com>
2023-05-02 18:58:47 +0000

    Update build script

--- build-assets.sh
+++ build-assets.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+export NVM_DIR="/usr/local/share/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd _keenthemes/tools/
 # . /usr/local/share/nvm/nvm.sh
@@ -11,6 +14,6 @@ nvm use 18
 npm install
 npm run build
 cd ../..
-mkdir static -p
-cp assets/* static/ -r
+mkdir static_build -p
+cp assets/* static_build/ -r
 rm assets -r
