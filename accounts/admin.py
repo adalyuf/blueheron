854f6f26d82f5c4d9e36eeca20519041f9e696c6
adalyuf <adalyuf@gmail.com>
2023-05-17 21:16:05 +0000

    Fix admin search with collations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.db.models.functions import Collate
 
 from .models import User
 
@@ -15,6 +16,17 @@ class UserAdmin(UserAdmin):
                     'fields': ('username', 'email', 'password1', 'password2'),
                 },
             ),
+        )
+    search_fields = ("email_deterministic","username_deterministic")
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .annotate(
+                email_deterministic=Collate("email", "und-x-icu"),
+                username_deterministic=Collate("username", "und-x-icu"),
+            )
         )
 
 admin.site.unregister(User)
