a34d27bcc2520b4271a869204b5bd18a1b84caf5
adalyuf <adalyuf@gmail.com>
2023-08-05 15:23:43 +0000

    Remove worker level max tasks

--- start-production-express.sh
+++ start-production-express.sh
@@ -12,7 +12,7 @@ celery --app=topranks worker --loglevel=info --detach --queues steamroller,expre
 gunicorn topranks.wsgi -b 0.0.0.0:80 --worker-tmp-dir /dev/shm --workers=2 --threads=4 --worker-class=gthread
 
 #To monitor celery in production, run flower locally with below command.
-#celery --app=topranks --broker=redis://:TopRedisPass99@18.219.28.43:6379 flower --port=5555
+#celery --app=topranks --broker=redis://:TopRedisPass99@18.219.28.43:6379 flower --port=5566
 
 #To have local machine help production queue, edit the devcontainer/docker-compose to use aws-production.env file and rebuild container
 #Then rebuild container and start celery worker with below command
